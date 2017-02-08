7bd0be239c08df0efae5d22090f7e0c6ccdab437
markusneumann <mneumann.polsci@gmail.com>
2017-02-08 15:27:20 -0500

    update to include census data

--- govWebsitesVerification.R
+++ govWebsitesVerification.R
@@ -1,6 +1,9 @@
-setwd("D:/Dropbox/4_RA/govWebsites")
+#setwd("D:/Dropbox/4_RA/govWebsites")
 setwd("/home/markus/Dropbox/4_RA/govWebsites")
+#setwd(getSrcDirectory(function(x) {x})) #set wd to .R file directory
 
+
+#read in the data created by the python scripts
 results1 <- read.table("results/results1.txt")
 results2 <- read.table("results/results2.txt")
 results3 <- read.table("results/results3.txt")
@@ -8,30 +11,33 @@ results4 <- read.table("results/results4.txt")
 results5 <- read.table("results/results5.txt")
 results6 <- read.table("results/results6.txt")
 
+#crete one data frame
 result <- rbind(results1,results2,results3,results4,results5,results6)
+rm(results1,results2,results3,results4,results5,results6)
 
+#read in original RSA file
 data <- read.csv("data/current-full.csv")
 
+#variable indicating where the .gov addresses redirect to
 data$redirect <- as.character(result$V1)
+rm(result)
 
-#devtools::install_github("jayjacobs/tldextract")
-#library(tldextract)
-#data$tld <- tldextract(data$redirect.V1)
-
+#did the website not load due to timeout
 data$timeout <- 0
 data$timeout[data$redirect=="TimeoutException"] <- 1
 
+#did the website not load at all?
 data$dead <- 0
 data$dead[data$redirect=="WebDriverException"] <- 1
 
+#set redirect to NA if either of those happened
 data$redirect[data$redirect%in%c("TimeoutException","WebDriverException")] <- NA
 
+#did the website load correctly?
 data$loaded <- 0
 data$loaded[is.na(data$redirect)==F] <- 1
 
-save(data, file="data/govWebsitesVerified.Rdata")
-
-xtabs(~loaded+Domain.Type, data=data)
+#xtabs(~loaded+Domain.Type, data=data)
 #loaded City County Federal Agency Native Sovereign Nation State/Local Govt
 #0  292    100            301                      39              221
 #1 2133    497           1030                     130              903
@@ -45,4 +51,40 @@ xtabs(~loaded+Domain.Type, data=data)
 #16,519 township governments
 #3.033 county governments
 
-#citytown.info
+
+#read in census data
+#Source:
+#http://www2.census.gov/programs-surveys/popest/datasets/2010-2015/cities/totals/sub-est2015_all.csv
+censusdata <- read.csv("data/sub-est2015_all.csv")
+censusdata <- censusdata[order(censusdata$STNAME,censusdata$NAME),]
+
+#in the census data, names always end with "town" or "city", removing
+censusdata$NAME <- sub(" town$", "", censusdata$NAME)
+censusdata$NAME <- sub(" city$", "", censusdata$NAME)
+
+#create variable with state abbreviations
+censusdata$StateShort <- state.abb[match(censusdata$STNAME, state.name)]
+
+#create StateCity variable for merge
+censusdata$StateCity <- paste(censusdata$StateShort, censusdata$NAME, sep="")
+
+#create StateCity variable for merge
+data$StateCity <- paste(data$State,data$City,sep="")
+
+#Remove everything but cities
+data <- subset(data, Domain.Type=="City")
+
+#merge census data and verified .gov addresses
+data <- merge(data, censusdata, by="StateCity", all.x=T, all.y = F)
+
+#remove duplicates (no relevant information is lost)
+data <- data[!duplicated(data$StateCity),]
+
+#remove any city whose website didn' load
+data <- subset(data, loaded==1)
+
+#remove any city that didn't get merged
+data <- data[is.na(data$CENSUS2010POP)==F,]
+
+#save results
+save(data, file="data/govWebsitesVerified.Rdata")
--- makefile
+++ makefile
@@ -8,11 +8,14 @@ verify:
 	python2 govWebsitesVerification_2701_3600.py
 	python2 govWebsitesVerification_3601_4500.py
 	python2 govWebsitesVerification_4501_5647.py
-	R CMD BATCH govWebsitesVerification.R
 
 #same as above, but run 3 webdrivers at the same time; faster
 #depending on fast internet connection, 6 should be possible too
 verify_parallel:
 	python2 govWebsitesVerification_1_900.py&	python2 govWebsitesVerification_901_1800.py&	python2 govWebsitesVerification_1801_2700.py
 	python2 govWebsitesVerification_2701_3600.py&	python2 govWebsitesVerification_3601_4500.py&	python2 govWebsitesVerification_4501_5647.py
+
+#assembles the output from the python scripts into a data frame
+#and merges it with census data on cities
+merge_census:
 	R CMD BATCH govWebsitesVerification.R
