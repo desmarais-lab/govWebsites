81e02bd4761294d585aa73321f26aad2d75d60a7
markusneumann <mneumann.polsci@gmail.com>
2017-06-19 19:01:39 -0400

    code to run LDA with mallet + results figure (word-topic probabilities) added to manuscript. I only added the one for the wget website content, the wayback machine ones look strange because they contain words that are too long (and also do not exist)

---
+++ mallet.R
@@ -0,0 +1,234 @@
+#This option has to be set BEFORE loading the rJava package (loaded by mallet)
+#Gives rJava about 8GB to work with
+#So far, I've only needed 2.3GB
+options(java.parameters = "-Xmx8000m")
+
+library('mallet')
+library('tibble')
+library('stringr')
+library('tools')
+library('tm')
+library('ggplot2')
+
+#setwd("./websites/websites")
+#setwd("~/RA4_website_archive/websites/current")
+
+f <- list.files(recursive = T) #create a list of all files in all subdirectories
+
+#file types
+ext <- file_ext(f) #get file extension
+folder <- str_split(f, "\\/(?=[^\\/]+$)", simplify = T)[,1]
+filename <- str_split(f, "\\/(?=[^\\/]+$)", simplify = T)[,2]
+
+d <- tibble(f, folder, filename, ext)
+
+#read in file text
+a <- str_c(readLines(d$f[1]), sep = " ", collapse = " ")
+#a
+d$a <- sapply(d$f, function(x){str_c(readLines(x), sep = " ", collapse = " ")})
+
+d$a <- as.character(d$a)
+
+#write stopwords list from the tm package to a text file
+fileConn <- file("../../stopwords.txt")
+writeLines(stopwords("english"), fileConn)
+close(fileConn)
+
+################################################################################
+
+#Mallet
+
+#Import website text
+mallet.instances <- mallet.import(id.array = make.unique(d$folder),
+                                  text.array = d$a,
+                                  stoplist.file = "../../stopwords.txt",
+                                  token.regexp = "\\p{L}[\\p{L}\\p{P}]+\\p{L}")
+
+#
+n.topics <- 100
+topic.model <- MalletLDA(n.topics)
+
+## Load our documents. We could also pass in the filename of a 
+##  saved instance list file that we build from the command-line tools.
+topic.model$loadDocuments(mallet.instances)
+
+## Get the vocabulary, and some statistics about word frequencies.
+##  These may be useful in further curating the stopword list.
+vocabulary <- topic.model$getVocabulary()
+vocabulary
+
+word.freqs <- mallet.word.freqs(topic.model)
+word.freqs
+
+## Optimize hyperparameters every 20 iterations, 
+##  after 50 burn-in iterations.
+topic.model$setAlphaOptimization(20, 50)
+
+## Now train a model. Note that hyperparameter optimization is on, by default.
+##  We can specify the number of iterations. Here we'll use a large-ish round number.
+topic.model$train(200)
+
+#R seems to be using about 2.3GB while training the model
+#Total time: 6 minutes 3 seconds
+
+## NEW: run through a few iterations where we pick the best topic for each token, 
+##  rather than sampling from the posterior distribution.
+topic.model$maximize(10)
+
+## Get the probability of topics in documents and the probability of words in topics.
+## By default, these functions return raw word counts. Here we want probabilities, 
+##  so we normalize, and add "smoothing" so that nothing has exactly 0 probability.
+doc.topics <- mallet.doc.topics(topic.model, smoothed=T, normalized=T)
+topic.words <- mallet.topic.words(topic.model, smoothed=T, normalized=T)
+
+## What are the top words in topic 7?
+##  Notice that R indexes from 1, so this will be the topic that mallet called topic 6.
+mallet.top.words(topic.model, topic.words[5,])
+
+## Show the first few documents with at least 5
+#head(documents[ doc.topics[7,] > 0.05 & doc.topics[10,] > 0.05, ])
+
+## How do topics differ across different sub-corpora?
+#nips.topic.words <- mallet.subset.topic.words(topic.model, documents$class == "NIPS",
+#                                              smoothed=T, normalized=T)
+#cvpr.topic.words <- mallet.subset.topic.words(topic.model, documents$class == "CVPR",
+#                                              smoothed=T, normalized=T)
+
+## How do they compare?
+#mallet.top.words(topic.model, nips.topic.words[10,])
+#mallet.top.words(topic.model, cvpr.topic.words[10,])
+
+
+
+
+#Plot top words for topics
+df.words <- tibble()
+
+for(i in 1:18){
+  df.words2 <- as.tibble(mallet.top.words(topic.model, topic.words[i,]))
+  df.words2$topic <- str_c("topic_", str_pad(i, 2, pad = "0"))
+  df.words <- rbind(df.words, df.words2)
+}
+
+
+
+#Word-topic probabilities
+plotWTP_01 <- df.words %>% ggplot(aes(words, weights, fill = factor(topic))) +
+  geom_bar(stat = "identity", show.legend = FALSE) +
+  facet_wrap(~ topic, scales = "free", ncol = 3) +
+  coord_flip() +
+  labs(title="Word-topic probabilities - before the election")
+plotWTP_01
+
+ggsave(plotWTP_01, file="../../wtp_before.pdf", width=7, height=9)
+
+#
+
+
+
+
+# from http://www.cs.princeton.edu/~mimno/R/clustertrees.R
+## transpose and normalize the doc topics
+#topic.docs <- t(doc.topics)
+#topic.docs <- topic.docs / rowSums(topic.docs)
+
+## Get a vector containing short names for the topics
+topics.labels <- rep("", n.topics)
+for (topic in 1:n.topics) topics.labels[topic] <- paste(mallet.top.words(topic.model, topic.words[topic,], num.top.words=5)$words, collapse=" ")
+# have a look at keywords for each topic
+topics.labels
+
+topic_docs <- data.frame(topic.docs)
+names(topic_docs) <- d$folder
+
+#
+# find top n topics for a certain author
+df1 <- t(topic_docs[,grep("pdf", names(topic_docs))])
+colnames(df1) <- topics.labels
+require(reshape2)
+topic.proportions.df <- melt(cbind(data.frame(df1),
+                                   document=factor(1:nrow(df1))),
+                             variable.name="topic",
+                             id.vars = "document") 
+# plot for each doc by that author
+require(ggplot2)
+ggplot(topic.proportions.df, aes(topic, value, fill=document)) +
+  geom_bar(stat="identity") +
+  ylab("proportion") +
+  theme(axis.text.x = element_text(angle=90, hjust=1)) +  
+  coord_flip() +
+  facet_wrap(~ document, ncol=5)
+
+
+## cluster based on shared words
+plot(hclust(dist(topic.words)), labels=topics.labels)
+
+
+## How do topics differ across different years?
+
+topic_docs_t <- data.frame(t(topic_docs))
+topic_docs_t$year <- documents$class
+# now we have a data frame where each row is a topic and 
+# each column is a document. The cells contain topic 
+# proportions. The next line computes the average proportion of
+# each topic in all the posts in a given year. Note that in 
+# topic_docs_t$year there is one FALSE, which dirties the data
+# slightly and causes warnings
+df3 <- aggregate(topic_docs_t, by=list(topic_docs_t$year), FUN=mean)
+# this next line transposes the wide data frame created by the above
+# line into a tall data frame where each column is a year. The 
+# input data frame is subset using the %in% function 
+# to omit the last row because this
+# last row is the result of the anomalous FALSE value that 
+# is in place of the year for one blog post. This is probably
+# a result of a glitch in the blog page format. I also exclude
+# the last column because it has NAs in it, a side-effect of the
+# aggregate function above. Here's my original line:
+# df3 <- data.frame(t(df3[-3,-length(df3)]), stringsAsFactors = FALSE)
+# And below is an updated version that generalises this in case 
+# you have more than two years:
+years <- sort(as.character(na.omit(as.numeric(as.character(unique(topic_docs_t$year))))))
+df3 <- data.frame(t(df3[(df3$Group.1 %in% years),-length(df3)]), stringsAsFactors = FALSE)
+# now we put on informative column names
+# names(df3) <- c("y2012", "y2013")
+# Here's a more general version in case you have more than two years
+# or different years to what I've got:
+names(df3) <- unname(sapply(years, function(i) paste0("y",i)))
+# the next line removes the first row, which is just the years
+df3 <- df3[-1,]
+# the next line converts all the values to numbers so we can 
+# work on them
+df3 <- data.frame(apply(df3, 2, as.numeric, as.character))
+df3$topic <- 1:n.topics
+
+# which topics differ the most between the years? 
+
+# If you have 
+# more than two years you will need to do things differently
+# by adding in some more pairwise comparisons. Here is one 
+# pairwise comparison:
+df3$diff <- df3[,1] - df3[,2] 
+df3[with(df3, order(-abs(diff))), ]
+# # then if you had three years you might then do
+# # a comparison of yrs 1 and 3
+# df3$diff2 <- df3[,1] - df3[,3] 
+# df3[with(df3, order(-abs(diff2))), ]
+# # and the other pairwise comparison of yrs 2 and 3
+# df3$diff3 <- df3[,2] - df3[,3] 
+# df3[with(df3, order(-abs(diff3))), ]
+## and so on
+
+
+# plot
+library(reshape2)
+# we reshape from long to very long! and drop the 
+# 'diff' column that we computed above by using a negatve 
+# index, that's the -4 in the line below. You'll need to change
+# that value if you have more than two years, you might find
+# replacing it with -ncol(df3) will do the trick, if you just
+# added one diff column. 
+df3m <- melt(df3[,-4], id = 3)
+ggplot(df3m, aes(fill = as.factor(topic), topic, value)) +
+  geom_bar(stat="identity") +
+  coord_flip()  +
+  facet_wrap(~ variable)
