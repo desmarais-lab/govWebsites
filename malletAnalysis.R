44f87f760556eec0c7f1d03cc2b730a46a43f9a3
markusneumann <mneumann.polsci@gmail.com>
2017-07-25 17:22:36 -0400

    added more analysis of topic weights

--- malletAnalysis.R
+++ malletAnalysis.R
@@ -102,6 +102,40 @@ democratic <- doc.topics[which(d$winner == "Democratic"),]
 republican <- as.tibble(republican)
 democratic <- as.tibble(democratic)
 
+## For each topic, calculate the median (or mean) weight across documents
+# i.e., on average (or median), documents contain x percent of words belonging to topic y
+rep.topic.prop <- apply(republican, 2, median)
+dem.topic.prop <- apply(democratic, 2, median)
+
+## Plot distribution of both
+weights.tibble <- tibble(Republican = rep.topic.prop, Democratic = dem.topic.prop) %>%
+  gather(Party, weight)
+topicweights_density <- ggplot(weights.tibble, aes(weight, color = Party)) + 
+  geom_density(size=1) + 
+  theme(legend.position="top") +
+  scale_color_manual(values=c("#4286f4", "#f44441")) +
+  ggtitle("Median document")
+#topicweights_density
+#ggsave(topicweights_density, file = str_c("./paper/figures/topicweights_density.pdf"), width=9, height=7)
+
+#Do the same thing, but with the mean instead of the median
+rep.topic.prop2 <- apply(republican, 2, mean)
+dem.topic.prop2 <- apply(democratic, 2, mean)
+## Plot distribution of both
+weights.tibble2 <- tibble(Republican = rep.topic.prop2, Democratic = dem.topic.prop2) %>%
+  gather(Party, weight)
+topicweights_density2 <- ggplot(weights.tibble2, aes(weight, color = Party)) + 
+  geom_density(size=1) + 
+  theme(legend.position="top") +
+  scale_color_manual(values=c("#4286f4", "#f44441")) +
+  ggtitle("Mean document")
+#topicweights_density2
+#ggsave(topicweights_density2, file = str_c("./paper/figures/topicweights_density2.pdf"), width=9, height=7)
+
+#Plot them together
+topicweights_density3 <- plot_grid(topicweights_density, topicweights_density2)
+topicweights_density3
+ggsave(topicweights_density3, file = str_c("./paper/figures/topicweights_density.pdf"), width=9, height=5)
 
 ## Plot the densities of weights across documents for each topic
 # i.e. each line is a topic
@@ -133,38 +167,24 @@ ggsave(doc.topics.dens.plot.g, file = str_c("./paper/figures/doctopics_density.p
 # as each topic has more very low values (i.e. most documents don't contain this topic), but also longer tails,
 # (i.e. a document that belongs to a specific topic)
 
-
-## For each topic, calculate the median (or mean) weight across documents
-# i.e., on average (or median), documents contain x percent of words belonging to topic y
-rep.topic.prop <- apply(republican, 2, median)
-dem.topic.prop <- apply(democratic, 2, median)
-#rep.topic.prop <- apply(republican, 2, mean)
-#dem.topic.prop <- apply(democratic, 2, mean)
-
-## Plot distribution of both
-weights.tibble <- tibble(Republican = rep.topic.prop, Democratic = dem.topic.prop) %>%
-  gather(Party, weight)
-
-topicweights_density <- ggplot(weights.tibble, aes(weight, color = Party)) + 
-  geom_density() + theme(legend.position="top")
-topicweights_density
-
-ggsave(topicweights_density, file = str_c("./paper/figures/topicweights_density.pdf"), width=9, height=7)
-
-
 ## For each topic, calculate the absolute difference
 # between the weight of this topic in Democratic and Republican cities/documents
 topicdiffs <- abs(dem.topic.prop-rep.topic.prop) %>% tibble(diffs = .)
+#topicdiffs <- abs(dem.topic.prop2-rep.topic.prop2) %>% tibble(diffs = .)
 
 ## Plot
-ggplot(topicdiffs, aes(diffs)) + geom_histogram(bins = 100)
+diffsize <- ggplot(topicdiffs, aes(diffs)) + 
+  geom_histogram(bins = 100) +
+  labs(x = "Size of differences")
+diffsize
+ggsave(diffsize, file = str_c("./paper/figures/diffsize.pdf"), width=9, height=7)
 
-#hist(topicdiffs)
-#plot(density(topicdiffs))
-
-#which(topicdiffs>.05)
-
+#get the biggest differences
+#>95th percentile
 bigdiffs <- which(topicdiffs > quantile(topicdiffs$diffs, .95))
+#10 biggest
+bigdiffs <- match(topicdiffs[order(topicdiffs$diffs, decreasing = T),]$diffs[1:10], topicdiffs$diffs) %>%
+  .[is.na(.)==F]
 
 #dem.topic.prop[bigdiffs]
 #rep.topic.prop[bigdiffs]
@@ -173,24 +193,24 @@ bigdiffs <- which(topicdiffs > quantile(topicdiffs$diffs, .95))
 df.words <- tibble()
 
 for(i in 1:length(bigdiffs)){
-  df.words2 <- as.tibble(mallet.top.words(topic.model, topic.words[i,]))
-  df.words2$topic <- str_c("topic_", str_pad(i, 2, pad = "0"))
+  df.words2 <- as.tibble(mallet.top.words(topic.model, topic.words[bigdiffs[i],]))
+  df.words2$topic <- str_c("topic_", str_pad(bigdiffs[i], 2, pad = "0"), " (", round(topicdiffs$diffs[bigdiffs[i]], digits = 6),")")
   df.words <- rbind(df.words, df.words2)
 }
 
 #Word-topic probabilities
-plotWTP_01 <- df.words %>% ggplot(aes(words, weights, fill = factor(topic))) +
+plotWTP_bigdiffs <- df.words %>% ggplot(aes(words, weights, fill = factor(topic))) +
   geom_bar(stat = "identity", show.legend = FALSE) +
-  facet_wrap(~ topic, scales = "free", ncol = 3) +
-  coord_flip() +
-  labs(title="Word-topic probabilities - wget")
-plotWTP_01
+  facet_wrap(~ topic, scales = "free", ncol = 2) +
+  coord_flip() #+
+  #labs(title="Word-topic probabilities - wget")
+plotWTP_bigdiffs
+
+ggsave(plotWTP_bigdiffs, file = str_c("./paper/figures/plotWTP_bigdiffs.pdf"), width=6, height=9)
 
 
-
-
-######
-
+## How do the topic weight density plots from above
+# look like just for the topics with big differences?
 doc.topics.dens.plot.dem <- gather(republican[,c(bigdiffs)])
 doc.topics.dens.plot.rep <- gather(democratic[,c(bigdiffs)])
 
@@ -199,13 +219,14 @@ doc.topics.dens.plot.rep$demtopicprop <- rep(dem.topic.prop[bigdiffs], each = nr
 
 doc.topics.dens.plot.dem.g <- ggplot(doc.topics.dens.plot.dem, aes(x = value, colour = key)) + 
   geom_density(size=.5, alpha=0.5) + 
-  xlim(0, 0.01) + ylim(0, 6000) +
+  xlim(0, 0.0021) + ylim(0, 6000) +
   ggtitle("Republican")
 doc.topics.dens.plot.rep.g <- ggplot(doc.topics.dens.plot.rep, aes(x = value, colour = key)) + 
   theme(legend.position="none") + 
   geom_density(size=.5, alpha=0.5) + 
-  xlim(0, 0.01) + ylim(0, 6000) +
+  xlim(0, 0.0021) + ylim(0, 6000) +
   ggtitle("Democratic")
 
 doc.topics.dens.plot.g <- plot_grid(doc.topics.dens.plot.dem.g, doc.topics.dens.plot.rep.g)
 doc.topics.dens.plot.g
+ggsave(doc.topics.dens.plot.g, file = str_c("./paper/figures/doctopics_density_bigdiffs.pdf"), width=10, height=6)
