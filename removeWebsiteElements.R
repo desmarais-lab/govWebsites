720e3a111abaf4479ccf66128f578566a3ffaa4c
markusneumann <mneumann.polsci@gmail.com>
2017-08-01 17:11:22 -0400

    finished script to calculate tf-idf for cities

--- removeWebsiteElements.R
+++ removeWebsiteElements.R
@@ -12,6 +12,10 @@ d <- d[d$spell_fail==0,]
 d$doc <- d$doc2
 d <- select(d, -doc2)
 
+d$doc <- gsub("\\s+"," ", d$doc)
+d$doc <- str_trim(d$doc, side = "both")
+d_bu <- d
+
 
 #calculate tf-idf for the entire corpus, with words as tokens
 dtv <- generate_document_term_vectors(d$doc[d$Name=="Attica"], tokenization_method = 'RegEx')
@@ -23,53 +27,52 @@ tf_idf <- tfidf(dtm, colnames(dtm))
 
 #TRYING to calculate tf-idf for n-grams
 #use corenlp
-a <- corenlp(d$doc[d$Name=="Attica"])
-save(a, file='rfiles/corenlp_attica.rdata')
-#get ngrams from corenlp output
-b <- ngrams(a)
-save(b, file='rfiles/corenlp_ngrams_attica.rdata')
-
-#get document term vectors
-#what is the expected input from the above?
-#dtv2 <- generate_document_term_vectors('rfiles/corenlp_ngrams_attica.rdata', 
-#                                       data_type = "ngrams", 
-#                                       ngram_type = "3_grams")
 
 
-NGrams <- b
+#table(d$Name), but not sorted by name
+citytable <- sapply(unique(d$Name), function(x){length(d$doc[d$Name==x])})
+ncities <- length(citytable)
+citynames <- names(citytable)
 
+for(k in 1:ncities){
 
-document_term_vector_list <- vector(mode = "list",
-                                    length = length(NGrams))
+  a <- corenlp(d$doc[d$Name==citynames[k]])
+  #save(a, file='rfiles/corenlp_attica.rdata')
+  #get ngrams from corenlp output
+  b <- ngrams(a)
+  #save(b, file='rfiles/corenlp_ngrams_attica.rdata')
+  
+  NGrams <- b
+  
+  document_term_vector_list <- vector(mode = "list",
+                                      length = length(NGrams))
+  
+  #modified function from SpeedReader package
+  for (i in 1:length(NGrams)) {
+    num <- 3
+    index <- grep(num, names(NGrams[[i]]$ngrams))[1]
+    document_term_vector_list[[i]] <- NGrams[[i]]$ngrams[[index]]
+  }
+  
+  #get tf-idf for the corpus of one city
+  dtm2 <- generate_document_term_matrix(document_term_vector_list)
+  tf_idf2 <- tfidf(dtm2, colnames(dtm2))
+  tf_idf3 <- tf_idf2$tfidf_rankings
+  tf_idf3 <- tf_idf3[order(tf_idf3$idf),]
+  tf_idf3 <- tf_idf3[tf_idf3$idf<1.5,]
+  tf_idf3$term <- str_replace_all(tf_idf3$term, "_", " ")
+  
+  citylength <- length(d$doc[d$Name==citynames[k]])
 
-for (i in 1:length(NGrams)) {
-  #  if (ngram_type == "jk_filtered") {
-  #    document_term_vector_list[[i]] <- NGrams[[i]]$jk_filtered
-  #  } else if (ngram_type == "verb_filtered") {
-  #    document_term_vector_list[[i]] <- NGrams[[i]]$verb_filtered
-  #  } else if (ngram_type == "phrases") {
-  #    document_term_vector_list[[i]] <- NGrams[[i]]$phrases
-  #  } else {
-  num <- 3
-  index <- grep(num, names(NGrams[[i]]$ngrams))[1]
-  document_term_vector_list[[i]] <- NGrams[[i]]$ngrams[[index]]
-  #  }
+  citystart <- 1
+  if(k>1){
+    citystart <- sum(citytable[1:(k-1)])+1
+  }
+  
+  for(j in citystart:(citystart+citylength-1)){
+    for(i in 1:length(tf_idf3$term)){
+      d$doc[j] <- str_c(clean_document_text(d$doc[j], regex = tf_idf3$term[i]), collapse = " ")
+    }
+  }
+
 }
-
-
-dtm2 <- generate_document_term_matrix(document_term_vector_list)
-tf_idf2 <- tfidf(dtm2, colnames(dtm2))
-tf_idf3 <- tf_idf2$tfidf_rankings
-tf_idf3 <- tf_idf3[order(tf_idf3$idf),]
-tf_idf3 <- tf_idf3[tf_idf3$idf<1.5,]
-tf_idf3$term <- str_replace_all(tf_idf3$term, "_", " ")
-
-d$doc <- gsub("\\s+"," ", d$doc)
-d$doc <- str_trim(d$doc, side = "both")
-
-for(i in 1:length(tf_idf3$term)){
-d$doc[1] <- str_c(clean_document_text(d$doc[1], regex = tf_idf3$term[i]), collapse = " ")
-}
-
-
-#sapply(tf_idf3$term, function(X){str_c(clean_document_text(d$doc[1], regex = X), collapse = " ")})
