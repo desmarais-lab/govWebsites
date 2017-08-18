b64a1c561e9b1955aea0de0b0a5726a7a459514d
markusneumann <mneumann.polsci@gmail.com>
2017-08-18 16:11:51 -0400

    added the results of finding the optimal number of topics, determined with the ldatuning package

--- ldatuning.R
+++ ldatuning.R
@@ -1,7 +1,11 @@
+#Code is basically from:
 #https://cran.r-project.org/web/packages/ldatuning/vignettes/topics.html
 
+##
+## NOTE: Running this script as it is takes about 50 hours on 11 cores
+##
+
 library("ldatuning")
-library("topicmodels")
 library('quanteda')
 
 #load data
@@ -13,16 +17,18 @@ dtm <- dfm(crps)
 
 result <- FindTopicsNumber(
   dtm,
-  topics = seq(from = 10, to = 100, by = 10),
-  metrics = c("Griffiths2004", "CaoJuan2009", "Arun2010", "Deveaud2014"),
+  topics = seq(from = 25, to = 500, by = 25),
+  metrics = c("Griffiths2004", "CaoJuan2009", "Arun2010"),
   method = "Gibbs",
   control = list(seed = 77),
   mc.cores = 11,
   verbose = TRUE
 )
 
-pdf("paper/figures/topicnumber_ldatuning.pdf")
+pdf("paper/figures/topicnumber_ldatuning.pdf", width = 9, height = 8, onefile = F)
 FindTopicsNumber_plot(result)
 dev.off()
 
-save.image("rfiles/topicmumber_ldatuning.Rdata")
+#save.image("rfiles/topicmumber_ldatuning2.Rdata")
+
+save(result, file = "rfiles/ldatuning_results.Rdata")
