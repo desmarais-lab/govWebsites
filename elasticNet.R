81f60096931c2c3e16ac874655c5c5329f831c5d
markusneumann <mneumann.polsci@gmail.com>
2017-09-22 23:44:06 -0400

    added elastic-net results (about 70% accuracy for in-sample prediction)

--- elasticNet.R
+++ elasticNet.R
@@ -1,4 +1,8 @@
 library('quanteda')
+library('Matrix')
+library('glmnet')
+
+#setwd('govWebsites')
 
 #load data
 load(file = "./rfiles/d.Rdata")
@@ -23,9 +27,23 @@ as.sparseMatrix <- function(simple_triplet_matrix_sparse) {
 dtm <- as.sparseMatrix(dtm)
 y <- as.numeric(as.factor(d$winner))-1
 
-##
 #use elastic-net in glmnet
-library('glmnet')
 
-glmnetFit <- glmnet(dtm, y, family = "binomial", maxit = 500000)
-summary(glmnetFit)
+#fit the model
+#raises a warning, but increasing maxit does not help
+glmnetFit <- glmnet(dtm, y, family = "binomial", maxit = 500000)  
+
+#in-sample prediction
+a = predict(glmnetFit, type = "class", dtm)
+#a = predict(glmnetFit, type = "response", dtm)
+
+#get majority vote on outcome from 45 factors
+a <- apply(a, 2, as.numeric)
+which.more <- function(X){
+  m <- as.numeric(names(table(X))[which.max(table(X))])
+  return(m)
+}
+a <- apply(a, 1, which.more)
+
+#percent correctly predicted
+mean(a==y)
