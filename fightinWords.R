2c2bf806755d7e7c67bc5ba24fae59ed7983cfc4
markusneumann <mneumann.polsci@gmail.com>
2017-09-23 18:32:54 -0400

    added implementation of Burts fightin words method through Matts SpeedReader package

---
+++ fightinWords.R
@@ -0,0 +1,44 @@
+library('quanteda')
+library('SpeedReader')
+
+#setwd('govWebsites')
+
+#load data
+load(file = "./rfiles/d.Rdata")
+
+#create corpus
+crps <- corpus(d$doc)
+
+#convert to dfm; then data frame
+dtm <- dfm(crps)
+dtm2 <- slam::as.simple_triplet_matrix(dtm)
+
+#create contingency table
+
+d$party <- d$winner
+
+cgt <- contingency_table(d, dtm2, variables_to_use = c('winner', 'party'))
+
+#fix the feature selection function
+source('functions/feature_selection_fixed.R')
+
+#throws a complaint about tf-idf which isnt even selected
+#fs <- feature_selection(cgt, rows_to_compare = c(1,4))
+
+#feature selection
+fs <- feature_selection(cgt, 
+                        rows_to_compare = c(4,1), #Democrat first so it gets colored blue
+                        method = "informed Dirichlet")
+
+#make the plot
+#pdf('paper/figures/fightinWords.pdf', width = 8, height = 8) #shitty quality
+ppi <- 300
+png("paper/figures/fightinWords.png", width=8*ppi, height=8*ppi, res=ppi)
+fightin_words_plot(fs, 
+                   title = "", 
+                   positive_category = 'Democrat', 
+                   negative_category = 'Republican',
+                   display_terms_next_to_points = T,
+                   size_terms_by_frequency = F,
+                   right_margin = 6)
+dev.off()
