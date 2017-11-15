d5983e12d2e2e44f6b1e6c541d61c579131305ab
markusneumann <mneumann.polsci@gmail.com>
2017-11-15 00:27:03 -0500

    improved hclust

--- hierarchicalClusteringIN.R
+++ hierarchicalClusteringIN.R
@@ -22,16 +22,19 @@ distMatrix <- dist(m, method="euclidean")
 #distMatrix <- cosine(t(m))
 
 # hierarchical clustering
-groups <- hclust(distMatrix, method="ward.D")
+groups <- hclust(distMatrix, method = "ward.D")
 groups$labels <- d$winner
 groups$labels[groups$labels=="Republican"] <- "R"
 groups$labels[groups$labels=="Democratic"] <- "D"
-hcd <- as.dendrogram(groups)
+groups2 <- groups
+groups2$height <- log(groups2$height)
+hcd <- as.dendrogram(groups2)
 # color by document party and plot
 colr <- rep("blue", nrow(d))
 colr[d$winner=="Republican"] <- "red"
 ppi <- 300
 png("paper/figures/clusteringIN.png", width=8*ppi, height=6*ppi, res=ppi)
+par(las = 1)
 hcd %>% set("labels_col", colr) %>% # change color
   set("labels_cex", .2) %>% # Change size
   plot()
@@ -39,3 +42,12 @@ dev.off()
 
 #everything takes quite a while, so save the results
 save.image("rfiles/hclustIN.rdata")
+
+entropy <- function(freqs){
+  -sum(freqs * log2(freqs))
+}
+
+for(i in 1:50){
+  test <- cutree(groups, i)
+  print(entropy(table(test)/length(test)))
+}
