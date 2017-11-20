91555c595abc50f4a7584d7305fd89e977055d7f
markusneumann <mneumann.polsci@gmail.com>
2017-11-20 15:49:56 -0500

    make 10 clusters, then attempt to manually label by looking at randomly sampled documents -- does NOT work well, the clusters seem totally, random, look at comments file for description of clusters 1 and 10

--- hierarchicalClusteringIN.R
+++ hierarchicalClusteringIN.R
@@ -43,11 +43,88 @@ dev.off()
 #everything takes quite a while, so save the results
 save.image("rfiles/hclustIN.rdata")
 
+#Shannon Entropy
 entropy <- function(freqs){
-  -sum(freqs * log2(freqs))
+  return(-sum(freqs * log2(freqs)))
 }
 
-for(i in 1:50){
-  test <- cutree(groups, i)
-  print(entropy(table(test)/length(test)))
+# =============================================================================
+
+#explore what the documents cluster into if k = 10
+nclust <- 10
+groups$labels <- 1:nrow(d)
+test <- cutree(groups, k = nclust)
+test.names <- names(test)
+cluster.categories <- data.frame(cluster = test, category = test.names)
+#test2 <- xtabs(~ category + cluster, data = cluster.categories)
+
+#for each cluster
+for(i in 1:nclust){
+
+clust.indices <- cluster.categories$category[cluster.categories$cluster==i]
+  
+ind <- sample(clust.indices, ifelse(length(clust.indices)>=10, 10, length(clust.indices)))
+
+#for each document within the cluster
+for(j in 1:length(ind)){
+
+  #get the document
+  current.doc <- d$doc[ind[j]]
+  #alternatively, read in original document
+  current.doc.original <- readLines(d$path[ind[j]])
+
+  separator <- "========================"
+  
+  # write three separators consisting of city and party
+  write(paste("\n", separator, d$Name[ind[j]], separator, d$winner[ind[j]], separator, "\n",
+              separator, d$Name[ind[j]], separator, d$winner[ind[j]], separator, "\n",
+              separator, d$Name[ind[j]], separator, d$winner[ind[j]], separator, "\n"), 
+        paste("./diagnostics/hclustering/cluster", i, ".txt", sep = ""), append = T)
+  
+  #write document
+  write(current.doc, paste("./diagnostics/hclustering/cluster", i, ".txt", sep = ""), append = T)
+
+
+  # write three separators consisting of city and party
+  write(paste("\n", separator, d$Name[ind[j]], separator, d$winner[ind[j]], separator, "\n",
+              separator, d$Name[ind[j]], separator, d$winner[ind[j]], separator, "\n",
+              separator, d$Name[ind[j]], separator, d$winner[ind[j]], separator, "\n"),
+        paste("./diagnostics/hclustering/original_cluster", i, ".txt", sep = ""), append = T)
+  #write document
+  write(current.doc.original, paste("./diagnostics/hclustering/original_cluster", i, ".txt", sep = ""), append = T)
+  
+  
+}}
+
+# =============================================================================
+
+#Clustering for cities
+
+groups$labels <- d$Name
+
+nclust <- 50
+
+entropy.values <- list()
+
+for(i in 1:nclust){
+  #cuttree to decide number clusters
+  test <- cutree(groups, k = i)
+  #party/city name
+  test.names <- names(test)
+  #cluster according to party/city
+  cluster.categories <- data.frame(cluster = test, category = test.names)
+  test2 <- xtabs(~ category + cluster, data = cluster.categories)
+  #add a bit of noise to the frequencies that are 0
+  test2[test2==0] <- test2[test2==0]+0.0000000000000001
+  #represent as proportions instead of frequencies
+  props <- as.numeric(test2/sum(test2))
+  #calculate entropy and save in list
+  entropy.values[[i]] <- entropy(props)
 }
+
+library("ggplot2")
+
+df <- data.frame(Entropy = unlist(entropy.values), Clusters = 1:nclust)
+
+ggplot(df, aes(x = Clusters, y = Entropy)) +
+  geom_point()
