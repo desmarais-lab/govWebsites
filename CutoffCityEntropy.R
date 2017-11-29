f856393b34c0ac08418da99353788e856f092f85
markusneumann <mneumann.polsci@gmail.com>
2017-11-28 22:54:50 -0500

    finally managed to get the cross city entropy for different duplicate lines removal cutoffs to work

--- CutoffCityEntropy.R
+++ CutoffCityEntropy.R
@@ -1,12 +1,16 @@
 
+city.entropies <- list()
+
 for(i in 1:20){
   
-  load("rfiles/docDuplicatesHash.Rdata")
-  load("rfiles/dIN2.rdata")
+  #load("rfiles/docDuplicatesHash.Rdata")
+  #load("rfiles/dIN2.rdata")
   
   cutoff <- i
   
-  source("functions/preprocessingTesting.R")
+  #source("functions/preprocessingTesting.R")
+  
+  load(paste0("./../Dropbox/govWebsitesMovingVan/d_test_K", cutoff, ".rdata"))
   
   source("functions/malletTraining.R")
   
@@ -16,7 +20,6 @@ for(i in 1:20){
   
   city.entr <- data.frame(x = city_entropy(doc.topics, d))
   
-  city.entropies <- list()
   city.entropies[[i]] <- city.entr
 
 }
@@ -24,17 +27,16 @@ for(i in 1:20){
 #create a density plot with one line for each cutoff value
 kCityEntropyVector <- unlist(city.entropies)
 #repeat the cutoff values, as many times as there are lines left at that cutoff
-kCutoffsVector <- rep(1:2, 200)
+kCutoffsVector <- rep(1:20, 200)
 #put in dataframe
 entropy.df <- data.frame(Entropy = kCityEntropyVector,
                          Cutoff = kCutoffsVector)
 
 save.image("rfiles/CutoffCityEntropy.rdata")
 
-#m1 <- ggplot(entropy.df, aes(x = kCityEntropyVector, 
-#                                   colour=kCutoffsVector, 
-#                                   group=kCutoffsVector))
-#m1 <- m1 + geom_density(fill = NA, size = .2, alpha = 0.1)
-#m1 <- m1 + xlim(0, 150)
-#m1 <- m1 + labs(x = "Line length (characters)", y = "Density", color = "Cutoff")
+m1 <- ggplot(entropy.df, aes(x = kCityEntropyVector, 
+                                   colour=kCutoffsVector, 
+                                   group=kCutoffsVector))
+m1 <- m1 + geom_density(fill = NA, size = .2, alpha = 0.1)
+m1 <- m1 + labs(x = "Entropy (between cities, for each topic)", y = "Density", color = "Cutoff")
 #ggsave(filename = "paper/figures/CutoffCityEntropy.pdf", plot = m1, width = 8, height = 6)
