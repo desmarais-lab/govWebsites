87ba3a5eb476d6581dcc269b3504caace40fb1e7
markusneumann <mneumann.polsci@gmail.com>
2018-02-02 09:45:45 -0500

    parallelize wget

--- wget.R
+++ wget.R
@@ -11,16 +11,25 @@ wget <- function(URLs, sites.dir){
   dir.create(sites.dir, recursive = T)
   
   #write URLs to a text file
-  readr::write_lines(x = URLs, path = paste0(sites.dir, "/URLs.txt"))
+  readr::write_lines(x = URLs, path = paste0(sites.dir, "/urls.txt"))
   
   #set new wd
   setwd(sites.dir)
   
   #scrape
-  system("wget -r -N -P ./ -i ./URLs.txt")
+  system("<urls.txt xargs -n 1 -P 8 -I % wget -r -N -P ./ %")
   
-  #clean up the text file created above
-  file.remove("URLs.txt")
+  #<urls.txt passes the file to xargs
+  #xargs -n 1 -P 8 starts 8 parallel processes
+  #-I % takes the input file and puts it into a variable %
+  #wget downloads websites
+  #-r recursive
+  #-N if re-doing the download, only get files if they are newer
+  #-P put everything into its own folders, in the current directory
+  # % takes the input file from above
+  
+  ###clean up the text file created above
+  ###file.remove("urls.txt")
 
 }
 
