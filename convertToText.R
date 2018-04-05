29dcc5ba9fe999e7455fcc97f7aa8a4c33f5ed51
markusneumann <mneumann.polsci@gmail.com>
2018-04-05 11:25:08 -0400

    converting html, doc, pdf, etc. to text

--- convertToText.R
+++ convertToText.R
@@ -2,11 +2,12 @@ library(stringr)
 library(tools)
 library(pbapply)
 library(readtext)
+library(doParallel)
 
 #function to convert every pdf, html, doc, or docx in a folder to text
 # and copy to a different folder
 
-convertToText <- function(path, newfolder = paste0(path, "TXT")){
+convertToText <- function(path){
   
   f <- list.files(path, recursive = T) #create a list of all files in all subdirectories
   f <- f[!stringr::str_detect(f, "[^\\x00-\\x7F]")]
@@ -28,8 +29,20 @@ convertToText <- function(path, newfolder = paste0(path, "TXT")){
   
   #only txt, pdf, html, doc, or docx
   d <- d[d$ext %in% c('txt', 'pdf', 'html', 'doc', 'docx'),]
+  
+  #d$path <- str_replace(d$path, "\\/\\/", "\\/")
+  
+  #remove some files that crash readtext
+  #everything in this folder causes some problems
+  d <- d[-which(stringr::str_detect(d$path, "/home/mneumann/hd2/govWebsites/bloomington.in.gov/trades/parcel/(.*?).pdf")),]
+  
+  d$path <- str_replace_all(d$path, "\\[", "\\\\[")
+  d$path <- str_replace_all(d$path, "\\]", "\\\\]")
+  
+  #save
   d$iter <- 1:nrow(d)
   save(d, file = "rfiles/doc_chunks/docnames.rdata")
+  
   
   #create new folder
   #d$newfolder <- str_replace(d$folder, path, newfolder)
@@ -45,16 +58,84 @@ convertToText <- function(path, newfolder = paste0(path, "TXT")){
   #newfile <- str_c(d$newfolder, "/", newfilename)
   
   #read in the old file
+  #chunk_size <- 10000
+  #for (i in seq(100001, nrow(d), chunk_size)) {
+  #  seq_size <- chunk_size
+  #  if ((i + seq_size) > nrow(d)) seq_size <- nrow(d) - i + 1
+  #  #for debugging, record which file we are on
+  #  #write(i, file="myfile.txt",append=TRUE)
+  #  a <- readtext(d$path[seq(i, (i+seq_size-1))])
+  #  a$iter <- seq(i, (i+seq_size-1))
+  #  save(a, file = paste0("rfiles/doc_chunks/parsedtexts_", i, "_", (i+seq_size-1), ".rdata"))
+  #  
+  #}
+  
+  #non-parallelized version of the code
+  
+  # chunk_size <- 10000
+  # for (i in seq(140001, nrow(d), chunk_size)) {
+  #   seq_size <- chunk_size
+  #   if ((i + seq_size) > nrow(d)) seq_size <- nrow(d) - i + 1
+  #   #for debugging, record which file we are on
+  #   #write(i, file="myfile.txt",append=TRUE)
+  #   
+  #   ab <- data.frame(doc_id = character(length = 0),
+  #                    text = character(length = 0),
+  #                    iter = numeric(length = 0))
+  #   
+  #   for(j in i:(i+seq_size-1)){
+  #     
+  #     try({
+  #       a <- readtext(d$path[j])
+  #       a$iter <- j
+  #       ab <- rbind(ab, a)
+  #     })
+  #     
+  #   }
+  #   
+  #   save(a, file = paste0("rfiles/doc_chunks/parsedtexts_", i, "_", (i+seq_size-1), ".rdata"))
+  #   
+  # }
+  
+  #parallelized version of the code
+  
   chunk_size <- 10000
   for (i in seq(1, nrow(d), chunk_size)) {
     seq_size <- chunk_size
     if ((i + seq_size) > nrow(d)) seq_size <- nrow(d) - i + 1
     
-    a <- readtext(d$path[seq(i, (i+seq_size-1))])
-    a$iter <- seq(i, (i+seq_size-1))
+    #register 11 parallel threads
+    registerDoParallel(cores=11)
+    #parallel loop over the documents in the current chunk
+    a <- foreach(j=i:(i+seq_size-1)) %dopar% {
+      
+      #error handling, in case readtext can't handle a specific document
+      try({
+        #readtext
+        a <- readtext(d$path[j])
+        #record the document number
+        a$iter <- j
+        #record the filepath
+        a$path <- d$path[j]
+      })
+      
+      #the foreach loop returns a 1x4 data.frame for each text
+      return(as.data.frame(a))
+      
+    }
+    
+    #kick the data frames out of the list that don't have three columns
+    broken <- -which(unlist(lapply(a, length))!=4)
+    if(length(broken)!=0){
+      a <- a[broken]
+    }
+    #combine the list of data frames from the foreach loop into one big data frame
+    a <- do.call(rbind, a)
+    
     save(a, file = paste0("rfiles/doc_chunks/parsedtexts_", i, "_", (i+seq_size-1), ".rdata"))
     
   }
+  
   
   
   #write out the new text files
@@ -66,4 +147,4 @@ convertToText <- function(path, newfolder = paste0(path, "TXT")){
   #}
 }
 
-convertToText("/home/mneumann/hd2/govWebsites/", "/home/mneumann/hd2/govWebsitesTXT/")
+convertToText("/home/mneumann/hd2/govWebsites")
