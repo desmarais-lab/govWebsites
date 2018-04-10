e0e6839a943c8c0de7945980e1ea4f5cd4d36fc5
markusneumann <mneumann.polsci@gmail.com>
2018-04-09 21:50:02 -0400

    wget failed to download the files for about 30 cities; try those again

---
+++ wgetReDo.R
@@ -0,0 +1,15 @@
+load("rfiles/citydocs_todo.rdata")
+
+sites.dir <- "/home/mneumann/hd2/cityWebsitesReDo"
+
+dir.create(sites.dir, recursive = T)
+
+#write URLs to a text file
+readr::write_lines(x = URLs, path = paste0(sites.dir, "/urls.txt"))
+
+filetype.remove <- paste("'*.flv'","'*.mov'","'*.swf'","'*.xml'","'*.js'","'*.css'","'*.zip'","'*.gz'","'*.rar'","'*.7z'","'*.tgz'","'*.tar'","'*.z'","'*.gzip'","'*.bzip'","'*.tar'","'*.mp3'","'*.mp4'","'*.aac'","'*.wav'","'*.au'","'*.wmv'","'*.avi'","'*.mpg'","'*.mpeg'","'*.xls'","'*.xlsx'","'*.ppt'","'*.pptx'","'*.jpg'","'*.jpeg'","'*.png'","'*.gif'","'*.psd'","'*.ico'","'*.bmp'","'*.odt'","'*.ods'","'*.odp'","'*.odb'","'*.odg'","'*.odf'", sep = ", ")
+
+#scrape
+wget <- paste0("<urls.txt xargs -n 1 -P 30 -I % wget -r -N -R '", filetype.remove, "' -P ./ %")
+
+readr::write_lines(x = URLs, path = paste0(sites.dir, "/wget.sh"))
