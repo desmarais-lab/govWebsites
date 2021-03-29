b5d5f7805d31a84b26f914837ae5e56dc1f6cc6a
markusneumann <m@ne.localdomain>
2021-03-29 15:09:28 -0400

    new partisanship plots for the stm

---
+++ 10_otherPlots/stm_partisanship_plots.R
@@ -0,0 +1,89 @@
+library(stm)
+library(ggplot2)
+
+load("../07_topicModel/stmSession_sim_60.rdata")
+
+topwords <- stm::labelTopics(stmFit, topics = NULL, n = 7, frexweight = 0.5)
+topwords <- topwords$frex
+
+# Republican
+plt_data <- plot(prep, covariate = "party",
+                 topics = c(20, 51, 2, 47, 19),
+                 model = stmFit,
+                 method = "difference",
+                 cov.value1 = "Republican",
+                 cov.value2 = "Democratic",
+                 xlim = c(-.002, .008),
+                 labeltype = "custom",
+                 custom.labels = c('Energy (Topic 20)',
+                                   'Fire protection (Topic 51)',
+                                   'Vaccination (Topic 2)',
+                                   'Sanitation (Topic 47)',
+                                   'Zoning (Topic 19)'),
+                 omit.plot = T)
+
+df_plt_R <- data.frame(topic = plt_data$topics,
+                       mean = unlist(plt_data$means),
+                       lower = do.call(rbind, plt_data$cis)[,1],
+                       upper = do.call(rbind, plt_data$cis)[,2],
+                       labels = plt_data$labels,
+                       topwords = apply(topwords[plt_data$topics,], 1, paste, collapse = " "))
+paletteR <- c("red3")
+ggplot(df_plt_R, 
+       aes(y = mean, x = nrow(df_plt_R):1, color = "red")) + 
+  geom_point(size=3) +
+  geom_hline(yintercept=0, color = "grey92", linetype = 2, size = .7) +
+  geom_errorbar(mapping=aes(ymin=lower, ymax=upper), size=1, width=0) + 
+  coord_flip() +
+  labs(y = "Estimated topic proportion for a Republican city", x = "", color = "Policy Area", shape = "Policy Area") +
+  theme_classic() + 
+  theme(legend.position = "none") +
+  geom_text(aes(label = topwords), size = 3.5, color = "black", alpha = 0.4, hjust=1.1) +
+  geom_text(aes(label = labels), size = 3.5, color = "black", hjust=-0.3) +
+  scale_colour_manual(values=paletteR) +
+  ylim(-0.011, 0.011) + theme(axis.text.y = element_blank(), axis.ticks.y = element_blank(), axis.line.y = element_blank())
+ggsave("../paper/figures/Republican_topics.pdf", width = 9, height = 5)
+
+# Democratic
+plt_data_D <- plot(prep, covariate = "party",
+                 topics = c(52, 6, 10, 29, 58, 45, 35, 55, 39),
+                 model = stmFit,
+                 method = "difference",
+                 cov.value1 = "Republican",
+                 cov.value2 = "Democratic",
+                 xlim = c(-.002, .008),
+                 labeltype = "custom",
+                 custom.labels = c('Affordable housing (Topic 52)',
+                                   'Race and gender (Topic 6)',
+                                   'Employee benefits (Topic 10)',
+                                   'Employee rights (Topic 29)',
+                                   'Public finances (Topic 58)',
+                                   'Public finances (Topic 45)',
+                                   'Public finances (Topic 35)',
+                                   'Public finances (Topic 55)',
+                                   'Mixed-use zoning  (Topic 39)'),
+                 omit.plot = T)
+
+df_plt_D <- data.frame(topic = plt_data_D$topics,
+                       mean = unlist(plt_data_D$means),
+                       lower = do.call(rbind, plt_data_D$cis)[,1],
+                       upper = do.call(rbind, plt_data_D$cis)[,2],
+                       labels = plt_data_D$labels,
+                       topwords = apply(topwords[plt_data_D$topics,], 1, paste, collapse = " "))
+
+paletteD <- c("#0072B2")
+ggplot(df_plt_D, 
+       aes(y = mean, x = nrow(df_plt_D):1, color = "blue")) + 
+  geom_point(size=3) +
+  geom_hline(yintercept=0, color = "grey92", linetype = 2, size = .7) +
+  geom_errorbar(mapping=aes(ymin=lower, ymax=upper), size=1, width=0) + 
+  coord_flip() +
+  labs(y = "Estimated topic proportion for a Republican city", x = "", color = "Policy Area", shape = "Policy Area") +
+  theme_classic() + 
+  theme(legend.position = "none") +
+  geom_text(aes(label = labels), size = 3.5, color = "black", hjust=1.2) +
+  geom_text(aes(label = topwords), size = 3.5, color = "black", alpha = 0.4, hjust=-0.1) +
+  scale_colour_manual(values=paletteD) +
+  ylim(-0.011, 0.011) + theme(axis.text.y = element_blank(), axis.ticks.y = element_blank(), axis.line.y = element_blank())
+ggsave("../paper/figures/Democratic_topics.pdf", width = 9, height = 5)
+
